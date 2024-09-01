4d80e6672823c3c27cd44a6c6c51dab7c69252c5
brandonflores <brandon@symph.co>
2024-09-01 23:48:17 +0800

    First commit

---
+++ apps/mobile/src/app/index.tsx
@@ -0,0 +1,62 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import React from 'react';
+import { LogBox } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import 'react-native-get-random-values';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { enableFreeze } from 'react-native-screens';
+import 'react-native-url-polyfill/auto';
+
+import { StorageManager } from '../components';
+import ApplicationRoutes from '../routes';
+import { tw } from '../tailwind';
+
+LogBox.ignoreLogs(['VirtualizedLists', 'onAnimatedValueUpdate']);
+
+enableFreeze(true);
+
+const CACHE_TIME = 0;
+const STALE_TIME = 0;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: STALE_TIME,
+    },
+  },
+});
+
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+});
+
+const persistOptions = { maxAge: CACHE_TIME, persister };
+
+function Application(): JSX.Element {
+  return (
+    <GestureHandlerRootView style={tw`flex-1`}>
+      <SafeAreaProvider>
+        <PersistQueryClientProvider client={queryClient} persistOptions={persistOptions}>
+          <StorageManager>
+            <ApplicationRoutes />
+          </StorageManager>
+        </PersistQueryClientProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function HeadlessCheck({ isHeadless }: any) {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
+
+  return <Application />;
+}
+
+export default HeadlessCheck;
---
+++ apps/mobile/src/routes/screen-options.ts
@@ -0,0 +1,5 @@
+
+export const screenOptions = {
+  gestureEnabled: true,
+  headerShown: false,
+};
